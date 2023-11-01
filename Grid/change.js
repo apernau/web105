f1eceb649e4cde34eeda35d4bc88780a1bb80418
apernau <142287559+apernau@users.noreply.github.com>
2023-11-01 00:31:01 -0500

    Add files via upload

---
+++ Grid/change.js
@@ -0,0 +1,15 @@
+function change01() {
+    document .getElementById("large").src = "images/blacklight-gimp.jpg";
+}
+function change02() {
+    document .getElementById("large").src = "images/eanda-gimp.jpg";
+}
+function change03() {
+    document .getElementById("large").src = "images/medievaltimes-gimp.jpg";
+}
+function change04() {
+    document .getElementById("large").src = "images/momandherboys-gimp.jpg";
+}
+function change05() {
+    document .getElementById("large").src = "images/shoes-gimp.jpg";
+}
