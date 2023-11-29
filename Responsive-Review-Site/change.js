7c824826bb612ca305a2b7d16bba511ad1e2af5e
apernau <142287559+apernau@users.noreply.github.com>
2023-11-28 23:35:43 -0600

    Add files via upload

---
+++ Responsive-Review-Site/change.js
@@ -0,0 +1,18 @@
+function change01() {
+    document.getElementById("large").src = "../images/plantain-sandwich.jpg";
+}
+function change02() {
+    document.getElementById("large").src = "../images/bruschetta.jpg";
+}
+function change03() {
+    document.getElementById("large").src = "../images/empanadas.jpg";
+}
+function change04() {
+    document.getElementById("medium").src = "../images/trifle.jpg";
+}
+function change05() {
+    document.getElementById("medium").src = "../images/chocolate-cake.jpg";
+}
+function change06() {
+    document.getElementById("medium").src = "../images/oreo-cupcakes.jpg";
+}
